78bdb89816366855d6bb307e6dac6bcdd7306d27
Karthikeyan S <103479792+karthdjd@users.noreply.github.com>
2023-02-19 00:07:24 +0530

    Update min_width_reset.v

--- Minimum Width Resetter/min_width_reset.v
+++ Minimum Width Resetter/min_width_reset.v
@@ -1,26 +1,26 @@
 module min_width_reset (
-    input clk,      // Clock
-    input rst_i,    // Synchronous Reset in
-    output rst_o    // Synchronous Reset out with min. pulse width assured
+    input clk,      
+    input rst_i,    
+    output rst_o   
 );
 
-parameter MIN_WIDTH = 4;   // Minimum pulse width of reset to be recognized, [2-16]
-parameter RST_POL = 0;     // Polarity of Synchronous Reset
+parameter MIN_WIDTH = 4;   
+parameter RST_POL = 0;    
 
-// Pulse Width Validator Chain : Synchronous Chain of Flip-Flops
+
 reg [MIN_WIDTH-1:0] sync_chain_rg = ~RST_POL;
 wire [MIN_WIDTH-1:0] temp_level0;
 wire [MIN_WIDTH-1:0] temp_level1;
 wire muxed_sync_rst;
 reg muxed_sync_rst_rg = RST_POL;
 
-// Clocked process
+
 always @(posedge clk) begin
     sync_chain_rg <= {sync_chain_rg[MIN_WIDTH-2:0], rst_i};
     muxed_sync_rst_rg <= muxed_sync_rst;
 end
 
-// Generate statement to self-OR and self-AND all bits of Synchronizer chain
+
 assign temp_level0[0] = sync_chain_rg[0];
 assign temp_level1[0] = sync_chain_rg[0];
 
@@ -32,10 +32,10 @@ generate
     end
 endgenerate
 
-// Muxed reset
+
 assign muxed_sync_rst = muxed_sync_rst_rg ? temp_level0[MIN_WIDTH-1] : temp_level1[MIN_WIDTH-1];
 
-// Reset out
+
 assign rst_o = muxed_sync_rst_rg;
 
 endmodule
